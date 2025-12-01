2b30c611da48ea5d85c22181830e9ae6446e6b2f
Dazzzzzz <102649215+JustinEmadem@users.noreply.github.com>
2025-12-02 02:15:54 +0800

    add roles shits

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
+use Spatie\Permission\Models\Role;
 use Illuminate\Http\Request;
 
 class RolesController extends Controller
