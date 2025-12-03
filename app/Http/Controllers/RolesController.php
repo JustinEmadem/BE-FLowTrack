c871a691b77b996a1c92ac4cab6cf2063f530ab2
Dazzzzzz <102649215+JustinEmadem@users.noreply.github.com>
2025-12-03 14:59:40 +0800

    make role request Image

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -3,6 +3,7 @@
 
 namespace App\Http\Controllers;
 use Spatie\Permission\Models\Role;
+use App\Http\Requests\RoleRequest;
 use Illuminate\Http\Request;
 
 class RolesController extends Controller
@@ -13,11 +14,9 @@ class RolesController extends Controller
         return response()->json($roles);
     }
 
-    public function createRole(Request $request)
+    public function createRole(RoleRequest $request)
     {
-        $request->validate([
-            'name' => 'required|string|unique:roles,name',
-        ]);
+        $validated = $request->validated();
         $role = Role::create(['name' => $request->name]);
         return response()->json($role, 201);
     }
